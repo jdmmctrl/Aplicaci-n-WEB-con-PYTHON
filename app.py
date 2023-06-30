6459dbb021254b1b9f7db6f7d583b1312fd1ed7a
jdmmctrl <juandavidmurillomejiaoutlook.es>
2023-06-30 09:39:32 -0500

    Actualizacion 30/06/2023/00

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import render_template, request, redirect
+from flaskext.mysql import MySQL
+from datetime import datetime
+
+app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'sistema'
+mysql.init_app(app)
+
+
+@app.route('/')
+def index():
+
+    sql = "SELECT * FROM empleados;"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    empleados = cursor.fetchall()
+    print(empleados)
+    conn.commit()
+
+    return render_template('empleados/index.html', empleados=empleados)
+
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM empleados WHERE id = %s", (id))
+    conn.commit()
+    return redirect('/')
+
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id = %s", (id))
+    empleados = cursor.fetchall()
+    conn.commit()
+    return render_template('empleados/edit.html', empleado=empleados)
+
+
+@app.route('/create')
+def method_name():
+
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _foto.filename != '':
+        nuevoNombre = tiempo + _foto.filename
+        _foto.save("uploads/" + nuevoNombre)
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+
+    datos = (_nombre, _correo, _foto.filename)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return render_template('empleados/index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
